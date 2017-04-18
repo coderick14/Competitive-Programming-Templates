27ebeb65501022456c01beb2d927c0dcff2a4a56
coderick14 <bhattacharyya.rick14@gmail.com>
2017-04-18 12:44:19 +0530

    Add function to print parantheses

--- matrix_chain.cpp
+++ matrix_chain.cpp
@@ -1,8 +1,26 @@
 #include <bits/stdc++.h>
+#define N 10
 using namespace std;
 
+int dp[N][N], parans[N][N];
+
+void printParans(int i, int j) {
+    if(i+1 == j) {
+        cout<<char('A'+i-1)<<char('A'+i);
+        return;
+    }
+    if(i == j) {
+        cout<<char('A'+i-1);
+        return;
+    }
+    cout<<'(';
+    printParans(i,parans[i][j]);
+    cout<<")(";
+    printParans(parans[i][j]+1,j);
+    cout<<')';
+}
+
 void matrixMult(int dimensions[], int n) {
-    int dp[n][n];
     int i,j,k,l;
 
     for(i=1; i<n; i++) {
@@ -14,12 +32,16 @@ void matrixMult(int dimensions[], int n) {
             dp[i][j] = INT_MAX;
             for(k=i; k<=j-1; k++) {
                 int cost = dp[i][k] + dp[k+1][j] + dimensions[i-1]*dimensions[k]*dimensions[j];
-                if(cost < dp[i][j])
+                if(cost < dp[i][j]) {
                     dp[i][j] = cost;
+                    parans[i][j] = k;
+                }
             }
         }
     }
     cout<<"Min cost : "<<dp[1][n-1]<<endl;
+    printParans(1,n-1);
+    cout<<endl;
 }
 
 int main() {
