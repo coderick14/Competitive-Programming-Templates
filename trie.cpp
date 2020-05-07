f7ee6f191300c90942dc103c5d1a6328b51431d0
coderick14 <bhattacharyya.rick14@gmail.com>
2020-05-07 07:07:53 +0530

    Update trie code

--- trie.cpp
+++ trie.cpp
@@ -2,55 +2,54 @@
 #define ll long long
 using namespace std;
 
-struct trie_node {
-    ll word_count;
-    ll prefix_count;
-    trie_node* edges[26];
+const int MAXN = 260005;
 
-    trie_node() {
-        this->word_count = 0;
-        this->prefix_count = 0;
-        for(int i = 0; i < 26; i++) {
-            this->edges[i] = NULL;
-        }
-    }
+struct arr {
+    int a[26];
+    arr() {}
+    void clear() { memset(a,-1,sizeof(a)); }
+    int& operator[](int i) { return a[i]; }
 };
 
-void add_word(trie_node* &root, string &str, int index) {
-    if (index == str.length()) {
-        root->word_count += 1;
-        return;
-    }
-    root->prefix_count += 1;
-    int i = int(str[index] - 'a'); 
-    if (root->edges[i] == NULL) {
-        root->edges[i] = new trie_node();
-    }
-    add_word(root->edges[i], str, index+1);
-}
+struct trie {
+    int cnt, prefix_cnt[MAXN], word_cnt[MAXN];
+    arr to[MAXN];
 
-ll count_prefix(trie_node* root, string &prefix, int index) {
-    if (index == prefix.length()) {
-        return root->prefix_count;
+    trie() { cnt = MAXN-1; }
+
+    void clear() { for(int i = 0; i < cnt; i++) prefix_cnt[i] = word_cnt[i] = 0, to[i].clear(); cnt = 1; }
+
+    void add(const string& s) {
+        int u = 0;
+        for(const char& c: s)  {
+            if(to[u][c-'a'] == -1) to[u][c-'a'] = cnt++;
+            u = to[u][c-'a'];
+            prefix_cnt[u]++;
+        }
+        word_cnt[u]++;
     }
-    int i = int(prefix[index] - 'a');
-    if (root->edges[i] == NULL) {
-        return 0;
+
+    int prefix_count(const string& s) {
+        int u = 0;
+        for (const char& c: s) {
+            if (to[u][c-'a'] == -1) return 0;
+            u = to[u][c-'a'];
+        }
+        return prefix_cnt[u];
     }
-    return count_prefix(root->edges[i], prefix, index+1);
-}
+} tr;
 
 int main() {
-    trie_node* root = new trie_node();
+    tr.clear();
     string s = "codechef";
-    add_word(root, s, 0);
+    tr.add(s);
     s = "codeforces";
-    add_word(root, s, 0);
+    tr.add(s);
     s = "youtube";
-    add_word(root, s, 0);
+    tr.add(s);
     s = "google";
-    add_word(root, s, 0);
+    tr.add(s);
     s = "code"; 
-    cout<<count_prefix(root, s, 0)<<endl;
+    cout<<tr.prefix_count(s)<<endl;
     return 0;
 }
